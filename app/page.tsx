ebe0f03380207b45f2f3e735a51d18043b86bef7
Ogieriakhi17 <osazeogieriakhi@gmail.com>
2026-02-19 01:53:47 -0800

    Created initial dashboard set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Page = () => {
-    return <div></div>
+    return <main className= "main-cointainer">
+        <section className="home-grid">
+            <div id="coin-overview">
+                <div className='header pt-2'>
+                    <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" 
+                    alt="Bitcoin" width={56} height={56}/ >
+                        <div className='info'>
+                            <p>Bitcoin / BTC</p>
+                            <h1>current_price</h1>
+                        </div>
+                </div>
+            </div>
+
+            <p>Trending Coin</p>
+        </section>
+
+        <section className="w-full mt-7 space-y-4">
+            <p>Categories</p>
+        </section>
+    </main>
 }
 export default Page
